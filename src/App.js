f319eeb2e8f1fb6e12f0fb502f2e903ec97f504f
Jason Klingstedt <jason.klingstedt@gmail.com>
2019-07-22 11:48:14 -0700

    basic app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            messages: [],
+            sseSource: null
+        }
+
+        this.subscribeToSSE = this.subscribeToSSE.bind(this)
+        this.stopSSE = this.stopSSE.bind(this)
+        this.clearMessages = this.clearMessages.bind(this)
+    }
+
+    componentDidMount() {
+        this.subscribeToSSE()
+    }
+
+    subscribeToSSE() {
+        this.setState(
+            {
+                sseSource: new EventSource('http://localhost:3001/event-stream')
+            },
+            () => {
+                this.state.sseSource.addEventListener('message', e => {
+                    this.setState({
+                        messages: [...this.state.messages, e.data]
+                    })
+                })
+            }
+        )
+    }
+
+    stopSSE() {
+        this.state.sseSource.close()
+        this.setState({ sseSource: null })
+    }
+
+    clearMessages() {
+        this.setState({ messages: [] })
+    }
+
+    render() {
+        return (
+            <>
+                <button
+                    onClick={
+                        this.state.sseSource
+                            ? this.stopSSE
+                            : this.subscribeToSSE
+                    }
+                >
+                    {this.state.sseSource ? 'Stop' : 'Start'}
+                </button>
+                <button onClick={this.clearMessages}>Clear Messages</button>
+                <ul>
+                    {this.state.messages.map(message => (
+                        <li key={message.split(' -- ')[1]}>{message}</li>
+                    ))}
+                </ul>
+            </>
+        )
+    }
 }
 
-export default App;
+App.propTypes = {}
+
+export default App
